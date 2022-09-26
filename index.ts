bd91d2ac0bc61864e7d47204184f4c7a7251fe13
Avery Yen <haplesshero13@gmail.com>
2022-09-26 17:13:32 -0400

    Add autoshoutout (hardcoded) and followage

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
 import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient, LogLevel } from '@twurple/chat';
+import { ApiClient } from '@twurple/api';
+import { formatDuration, intervalToDuration } from 'date-fns';
 import { readFile, writeFile } from 'node:fs/promises';
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
 async function main() {
+  const autoShoutoutUsers = [];
+  const autoShoutouts = autoShoutoutUsers.reduce<Record<string, Date | number>>((acc, user) => {
+    return {...acc, [user]: 0}},
+    {}
+  )
+
+  const shouldAutoShoutout = (user: string): boolean => {
+    const timestamp = autoShoutouts[user];
+
+    return timestamp != null &&
+      (intervalToDuration({start: timestamp, end: new Date()}).days ?? 0) > 0;
+  }
+
 	const clientId = process.env.CLIENT_ID ?? '';
 	const clientSecret = process.env.CLIENT_SECRET ?? '';
 	const tokenData = JSON.parse(await readFile('./tokens.json', 'utf8'));
@@ -18,31 +33,38 @@ async function main() {
 		tokenData
 	);
 
+  const apiClient = new ApiClient({ authProvider });
+
   const channels = ['haplesshero'];
 	const chatClient = new ChatClient({ authProvider, channels, logger: {minLevel: LogLevel.DEBUG} });
 	await chatClient.connect();
 
   chatClient.onJoin(channel => {
-    chatClient.say(channel, '/me has joined the chat!')
+    chatClient.say(channel, '/me has joined the chat! 🤖')
   });
 
-	chatClient.onMessage((channel, user, message) => {
-		if (message === '!ping') {
-			chatClient.say(channel, 'Pong!');
-		} else if (message === '!dice') {
-			const diceRoll = Math.floor(Math.random() * 6) + 1;
-			chatClient.say(channel, `@${user} rolled a ${diceRoll}`)
-		}
-	});
+	chatClient.onMessage(async (channel, user, message, msg) => {
+    if (shouldAutoShoutout(user)) {
+			chatClient.say(channel, `Go check out ${user}!`);
+    }
 
-	chatClient.onSub((channel, user) => {
-		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel!`);
-	});
-	chatClient.onResub((channel, user, subInfo) => {
-		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
-	});
-	chatClient.onSubGift((channel, user, subInfo) => {
-		chatClient.say(channel, `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
+    if (message === '!followage') {
+      const follow = await apiClient.users.getFollowFromUserToBroadcaster(msg.userInfo.userId, msg.channelId!);
+
+      if (follow) {
+        const currentTimestamp = new Date();
+        const followStartTimestamp = follow.followDate;
+        
+        console.log(JSON.stringify({start: followStartTimestamp, end: currentTimestamp}))
+
+
+        chatClient.say(channel, `@${user} You have been following for ${formatDuration(
+          intervalToDuration({start: followStartTimestamp, end: currentTimestamp}), { delimiter: ', ' }
+        )}!`);
+      } else {
+        chatClient.say(channel, `@${user} You are not following! smh`);
+      }
+    }
 	});
 }
 
