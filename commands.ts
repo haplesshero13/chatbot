4f84036b41fd5b0dbd956276be68f07666b556a9
Hapless Hero <haplesshero13@gmail.com>
2023-04-08 10:29:37 -0400

    Update node ver to 18, and add splatoon commands

--- commands.ts
+++ commands.ts
@@ -3,15 +3,61 @@ import { formatDuration, intervalToDuration } from 'date-fns';
 import { ChatClient } from '@twurple/chat';
 import { ApiClient } from '@twurple/api';
 
-export const commands = (apiClient: ApiClient, chatClient: ChatClient) => [
+type CommandImpl = (
+  args: string[],
+) => (
+  channel: string,
+  user: string,
+  message: string,
+  msg: TwitchPrivateMessage,
+) => Promise<void>;
+
+export type Command = {
+  pattern: RegExp;
+  implementation: CommandImpl;
+};
+
+const fetchSplatSchedule = (mode: string, args: string[]) => {
+  return fetch(
+    `http://schedule.splat.pw/splatoon3?q=${args[1]}&arg=${args[2]}&mode=${mode}`,
+  ).then((response) => response.text());
+};
+
+export const commands = (
+  apiClient: ApiClient,
+  chatClient: ChatClient,
+): Command[] => [
+  {
+    pattern: /^!anarchy/,
+    implementation: (args) => async (channel, user) => {
+      const response = await fetchSplatSchedule('bankara', args);
+      chatClient.say(channel, `@${user} -> ${response}`);
+    },
+  },
+  {
+    pattern: /^!regular/,
+    implementation: (args) => async (channel, user) => {
+      const response = await fetchSplatSchedule('regular', args);
+      chatClient.say(channel, `@${user} -> ${response}`);
+    },
+  },
+  {
+    pattern: /^!x/,
+    implementation: (args) => async (channel, user) => {
+      const response = await fetchSplatSchedule('x', args);
+      chatClient.say(channel, `@${user} -> ${response}`);
+    },
+  },
+  {
+    pattern: /^!league/,
+    implementation: (args) => async (channel, user) => {
+      const response = await fetchSplatSchedule('league', args);
+      chatClient.say(channel, `@${user} -> ${response}`);
+    },
+  },
   {
     pattern: /^!followage/,
-    implementation: async (
-      channel: string,
-      user: string,
-      message: string,
-      msg: TwitchPrivateMessage,
-    ) => {
+    implementation: () => async (channel, user, message, msg) => {
       const follow = await apiClient.users.getFollowFromUserToBroadcaster(
         msg.userInfo.userId,
         msg.channelId ?? '',
